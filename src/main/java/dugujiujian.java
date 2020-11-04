daeb87b25ba2a8fe637d0909602bd42e5da9ed17
linghuchong0523 <2450213331@qq.com>
2020-11-04 21:04:44 +0800

    令狐冲

--- src/main/java/dugujiujian.java
+++ src/main/java/dugujiujian.java
@@ -2,5 +2,6 @@ public class dugujiujian {
     public static void main(String[] args) {
         System.out.println("紫霞神功");
         System.out.println("华山论剑");
+        System.out.println("吸星大法");
     }
 }
